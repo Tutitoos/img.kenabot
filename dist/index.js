97a934d02bdba31f77427e7699e1059fa86bbad6
Tutitoos <tutitoosdev@gmail.com>
2022-09-04 20:21:42 +0200

    Update dist

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.delay = exports.arrayCache = void 0;
+require("dotenv/config");
+const discord_controller_1 = require("./controllers/discord.controller");
+const server_1 = require("./server");
+exports.arrayCache = [];
+const delay = (timeout) => __awaiter(void 0, void 0, void 0, function* () { return new Promise((resolve) => setTimeout(resolve, timeout)); });
+exports.delay = delay;
+let loading = false;
+(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield (0, server_1.server)();
+        if (!loading)
+            yield refreshData();
+        if (loading)
+            setInterval(() => refreshData(), 86400000);
+    });
+})();
+function refreshData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Refreshing data...");
+        for (const key of exports.arrayCache) {
+            const DATE_NOW = Date.now();
+            if (DATE_NOW >= key.createdAt) {
+                const { data } = yield (0, discord_controller_1.getUser)(key.id);
+                if (typeof data === "object" && data) {
+                    const findUserIndex = exports.arrayCache.findIndex((user) => user.id === key.id);
+                    exports.arrayCache[findUserIndex].createdAt = DATE_NOW + 86400000;
+                    exports.arrayCache[findUserIndex].avatar = data.avatar;
+                }
+            }
+        }
+        loading = true;
+        console.log("Refreshing data finished");
+    });
+}
---
+++ dist/routes.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const discord_controller_1 = require("./controllers/discord.controller");
+const index_1 = require("./index");
+const routes = (0, express_1.Router)();
+routes.get("/user/:userID", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userID } = req.params;
+    if (!userID) {
+        return res.status(200).json({
+            status: 200,
+            message: "Falta el parámetro userID",
+            data: null,
+        });
+    }
+    const userData = index_1.arrayCache.find((user) => user.id === userID);
+    if (!userData) {
+        let newUserData = undefined;
+        const DATE_NOW = Date.now();
+        const { data } = yield (0, discord_controller_1.getUser)(userID);
+        if (typeof data === "object" && data) {
+            newUserData = Object.assign({ createdAt: DATE_NOW + 86400000 }, data);
+        }
+        if (newUserData) {
+            console.log("New user added: ", newUserData.id);
+            index_1.arrayCache.push(newUserData);
+        }
+    }
+    res.status(200);
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    return res.send(userData === null || userData === void 0 ? void 0 : userData.avatar);
+}));
+exports.default = routes;
